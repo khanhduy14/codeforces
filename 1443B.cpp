16d0c3527cf1f7f57d732154dd38437c7e2c8b62
phongdatpirago <dat.phong-dev02@pirago.vn>
2020-11-11 15:48:42 +0700

    1443B

---
+++ 1443B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	cout<<"123123";
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
+}
