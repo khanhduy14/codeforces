47e531ebf85477112ede6e301b71228e4a889cbb
phongdatpirago <dat.phong-dev02@pirago.vn>
2020-11-11 16:38:38 +0700

    1443B finish

--- 1443B.cpp
+++ 1443B.cpp
@@ -1,8 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define ar array
+
+int a, b;
+
 void solve() {
-	cout<<"123123";
+	cin >> a >> b;
+	string s;
+	cin >> s;
+	int ans = a;
+	int first = -1;
+	int last = -1;
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if (s[i] == '1')	
+		{
+			first = i;
+			break;
+		}
+		 
+	}
+
+	for (int i = s.size() - 1; i >= 0; --i)
+	{
+		if (s[i] == '1')
+		{
+			last = i;
+			break;
+		}
+	}
+
+	if (first == last && first == -1)
+	{
+		/* code */
+		cout << 0 << "\n";
+		return;
+	}
+
+
+	for (int i = first; i <= last; ++i)
+	{
+		int c = 0;
+		 while(s[i] == '0') {
+		     c++;
+		     i++;
+		 }
+	 ans += min(a, b*c);
+	}
+	cout << ans << "\n";
 }
 
 int main() {
---
+++ default.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int a, b;
+
+void solve() {
+	cin >> a >> b;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
+}
